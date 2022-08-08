428e801d001adcd0044be636384601cf0506d044
Leo Luong Vuong <myluong.1710@gmail.com>
2022-08-08 17:56:55 +0200

    I make some changes
    Leo

---
+++ mainLeoupdate.R
@@ -0,0 +1,69 @@
+#First I tried to make some changes
+
+#Input Arguments
+n<-1000                   # Population size
+prop.immune <-0           # Proportion of immune individuals
+rho <-0.4                 # Probability of a symptomatic infection
+q <-0.5                   # Transmission potential
+alpha.as<- 0.1            # Relative infectiousness asymptomatic carriers
+testing.prob<-1           # Probability that a symptomatic individual is tested
+test.sens<-0.9            # Sensitivity of the test
+test.delay<-1             # Delay from taking the test to the test result
+contact.reduction<-0.1    # Reduction of contact rate
+nSeeds<-3                 # Number initial infected individuals
+lambda<-5                 # Number of daily contacts
+
+#running simulations
+source("scrLeo.R")
+nSim<-50
+set.seed(131714)
+epi.outbreak<-list()
+for (i in 1:nSim){
+  print(i)
+  epi.outbreak[[i]]<-sim.ekp(n=n,prop.immune = prop.immune,rho = rho,q=q,alpha.as = alpha.as,testing.prob = testing.prob,test.sens = test.sens,test.delay = test.delay,contact.reduction = contact.reduction,nSeeds = nSeeds, lambda = lambda)
+}
+
+
+
+
+# Plot Epidemiological quantities
+#Look at the evolution for a single epidemic
+single.ep<-epi.outbreak[[sample(1:nSim,1)]]
+plot(single.ep$epi.evo$Days, single.ep$epi.evo$Prevalence, type="l", xlab = "Days", ylab = "Prevalence")
+plot(single.ep$epi.evo$Days, single.ep$epi.evo$Incidence, col="red", xlab = "Days", ylab = " Incidence")
+plot(single.ep$epi.evo$Days, single.ep$epi.evo$Rt, col="blue", ylab = "Effective Reproductive Number", xlab = "Days" )
+
+#look at the evolution of different epidemics
+n.epidemics<-50 #Number of epidemics you want to consider
+epidemics<-sample(1:nSim,n.epidemics) #Make sure you are selecting a number of epidemics smaller than the number of simulations
+
+plot(epi.outbreak[[epidemics[1]]]$epi.evo$Days, epi.outbreak[[epidemics[1]]]$epi.evo$Prevalence, type="l", xlab = "Days", ylab = "Prevalence", xlim = c(0,130),ylim = c(0,30)) # you need to adjust the x and y axis, to see all the epidemics. To do so adjust xlim and ylim
+for (i in 1:n.epidemics){
+  lines(epi.outbreak[[epidemics[i]]]$epi.evo$Days, epi.outbreak[[epidemics[i]]]$epi.evo$Prevalence, col=i)
+}
+
+
+
+
+#Look at summary measures among different epidemics
+FinalSize<-NULL
+PeakIncidence<-NULL
+PeakPrevalence<-NULL
+
+for (i in 1:nSim){
+  FinalSize<-c(FinalSize,epi.outbreak[[i]]$FinalSize$FinalSize)
+  PeakIncidence<-c(PeakIncidence, epi.outbreak[[i]]$PeakIncidence$PeakIncidence)
+  PeakPrevalence<-c(PeakPrevalence,epi.outbreak[[i]]$PeakPrevalence$PeakPrevalence)
+}
+
+boxplot(FinalSize, ylab="Final Size")
+boxplot(PeakIncidence, ylab="Peak Incidence")
+boxplot(PeakPrevalence, ylab="Peak Prevalence")
+
+
+# Save Simulations
+name<-paste("EpiOutbreak", "_N",n,"_nSeeds",nSeeds,"_PropImm",prop.immune,"_rho",rho,"_q",q,"_alpha",alpha.as,"_testingProb",testing.prob,"_testSens",test.sens,"_testdelay",test.delay,"_contact reduction",contact.reduction,sep = "")
+setwd("D:/Hoc/Uantwerp/Infectious Disease Modelling Internship/scripts")
+save(epi.outbreak, file = paste(name,".RData",sep = ""))
+
+
---
+++ scrLeo.R
@@ -0,0 +1,251 @@
+###################################################
+# Functions used to assign:
+# - incubation period 
+# - infectious period
+# - infectivity Measure
+###################################################
+
+# The following functions are set to describe the spreading of SARS-CoV-2
+
+# We compute the day of symptom onset as the peak of the infectivity measure
+incubation.period<-function(lengthIP){
+  time.points<-seq(0,lengthIP,0.01)
+  infectiousnessmeasure.values<-(15/lengthIP)*dgamma(15/lengthIP*time.points, shape =12 , scale =0.48 )
+  time.max<-time.points[which(infectiousnessmeasure.values==max(infectiousnessmeasure.values))] 
+  return(time.max) 
+}
+
+infectious.period.length<-function(){
+  return(rgamma(1,shape = 32.14,scale = 0.47))#better to move to Gamma, e.g. Gamma with shape 26.01 scale 0.392 -> mean 10.2 and 2 sd
+}
+
+# Infectivity measure is set to represent viral load progression
+InfMeasure<-function(t,lengthIP){
+  vload.comp<-(15/lengthIP)*dgamma(15/lengthIP*t, shape = 12, scale = 0.48) #5.7 GT without interventions
+  return(vload.comp)
+}
+
+#########################3
+# Simulating Function
+#########################3
+
+sim.ekp<-function(n,prop.immune, rho,q, alpha.as,testing.prob,test.sens,test.delay,contact.reduction,lambda,nSeeds){
+  
+  
+  
+  status.matrix <- data.frame(infected          = rep(0,n), # status variable: indicate if individuals are infectious (1), susceptible (2) or recovered (3)
+                              time.of.infection = NA,       # time of infection
+                              infector          = NA,       # infector
+                              severity          = -1,       # severity: 1 symptomatic infection, 2 asymptomatic infection
+                              TimeSymptomOnset  = Inf,      # symptom onset date
+                              IPLength          = NA,       # length of infectious period
+                              Recovery          = Inf,      # recovery time 
+                              TimePosTest       = Inf)      # time at which the test gives positive result
+  
+  
+  
+  events<-data.frame("NextCtc"=Inf,"TestPositive"=Inf, "Recovery"=Inf)
+  
+  infectives<-rep(0,n) # vector that indicates who is infectious at the current time: 1 infectious 0 non infectious
+  current.time<-0
+  index.contact<-rep(0,n) # vector that selects the individuals that have to propose a new social contact (global) - 1 yes 0 no
+  time.events<-matrix(NA,1,3)
+  
+  #transmission parameter dataframe: each line is an individual, the first column is the transmission coefficient and the third is the length of IP (needed to re-scale Viral load curve)
+  transmission.parameters<-data.frame("id"=1:n,"q"=rep(q,n),"contact_rate"=rep(lambda,n))   #matrix containing the proposed time of the next contact (first column) and the contact individual (second column)
+  
+  #Proportion of immune
+  if (prop.immune>0){
+    status.matrix$infected[sample(1:n,round(prop.immune*n))]<--2 #-2 means they are immuned
+  }
+  
+  testpositive.day<-rep(Inf,n)
+  contact.time<-data.frame("id"=1:n,"pr.ctc"=rep(NA,n),"pr.infectee"=rep(NA,n))   #matrix containing the proposed time of the next contact (first column) and the contact individual (second column)
+  
+  # first infected: randomly chosen in the population (among the susceptibilities)
+  first.cases<-sample(which(status.matrix$infected==0),nSeeds)
+    
+  # update the infectious status and epidemiological characteristics of the seeded cases  
+  for (j in first.cases){
+    first<-j
+    status.matrix$infected[first] <- 1 
+    status.matrix$time.of.infection[first] <- 0
+    status.matrix$IPLength[first]<-infectious.period.length()
+    status.matrix$Recovery[first]<-current.time+status.matrix$IPLength[first]
+    if (runif(1)<rho){ #if symptomatic
+      status.matrix$severity[first]<-1
+      status.matrix$TimeSymptomOnset[first]<-current.time+incubation.period(status.matrix$IPLength[first])
+      if (runif(1)<testing.prob){ #a patient is tested (Leo add)
+        if (runif(1)<test.sens){ #the test is positive (Leo add)
+          status.matrix$TimePosTest[first]<-status.matrix$TimeSymptomOnset[first]+test.delay #assume only symptomatic individuals are tested (Leo add)
+        }
+      }
+      time.events<-rbind(time.events,c(current.time,1.1,first)) #what is time.events, why 1.1? (Leo ask)
+    }else{
+      status.matrix$severity[first]<-2
+      transmission.parameters$q[first]<-transmission.parameters$q[first]*alpha.as 
+      time.events<-rbind(time.events,c(current.time,1.2,first)) #what is time.events, why 1.2? (Leo ask)
+    }
+    infectives[first]<-1
+    contact.time$pr.ctc[first]<-rexp(1,transmission.parameters$contact_rate[first])+current.time       # Generate the next inter-arrival time for individual i #Is this the time of the next contact? (Leo ask)
+  }
+  
+  proposed.individual<-0
+  temp.contact.time<-0
+  indiv.prop.ctc<-0
+  recovered<-0
+  err<-0
+  
+  #When only the first pathogen is present #what is this (Leo ask)
+  while((sum(infectives))>0){ #while there are still infectives
+    #Phase 1: individuals that has to, propose a new social contact
+    for (i in which(index.contact==1) ){ # for all the individuals that has to propose a global contact    #why 1 because previously we assign index.contact to 0 (Leo ask)
+      contact.time$pr.ctc[i]<-rexp(1,transmission.parameters$contact_rate[i])+current.time# I generate the next interarrival time for individual i
+      index.contact[i]<-0 #i only propose 1 contact?
+    }
+    
+    next.contact<-contact.time$pr.ctc #overall contact times    
+    recovery.vector<-status.matrix$Recovery
+    testpositive.day<-status.matrix$TimePosTest
+    #Identify the next event (the one that is realized first in time)
+    ifelse(length(which(is.na(next.contact)==FALSE))>0,events$NextCtc<-min(next.contact, na.rm = T),events$NextCtc<-Inf) # among all the proposed social contact between households we select the minimum
+    ifelse(length(which(!is.infinite(testpositive.day)))>0,events$TestPositive<-min(testpositive.day),events$TestPositive<-Inf ) #
+    ifelse(length(which(is.na(recovery.vector)==FALSE))>0,events$Recovery<-min(recovery.vector, na.rm = T),events$Recovery<-Inf) # among all the proposed social contact between households we select the minimum
+
+    next.evts<-colnames(events)[which(min(events)==events)] # if two at the same time we pick one random
+    if (length(next.evts)>1){
+      next.evts<-sample(colnames(events)[which(min(events)==events)],1)
+    }
+    
+    if (next.evts=="NextCtc"){
+      current.time<-events$NextCtc
+      if (length(min(contact.time, na.rm = T))>1){ #when two contacts happen at the same time we select one at random
+        infector<-sample(which(contact.time==current.time),1) 
+        infectee<-sample(setdiff(1:n,infector),1)
+        index.contact[infector]<-1
+        contact.time$pr.ctc[infector]<-NA
+      }else{
+        infector<-which(contact.time$pr.ctc ==events$NextCtc)
+        infectee<-sample(setdiff(1:n,infector),1)
+        index.contact[infector]<-1
+        contact.time$pr.ctc[infector]<-NA
+      }
+      inf.ctc<-transmission.parameters$q[infector]
+      acc.rate<-InfMeasure(t=(current.time-status.matrix$time.of.infection[infector]), lengthIP = status.matrix$IPLength[infector])*inf.ctc
+      if (status.matrix$infected[infector]!=1){acc.rate<-0}
+      if (acc.rate>1){err<-err+1}
+      if (status.matrix$infected[infectee]==0 & runif(1)<acc.rate){
+        status.matrix$infected[infectee] <- 1 
+        status.matrix$time.of.infection[infectee] <-current.time
+        status.matrix$infector[infectee]<-infector
+        status.matrix$IPLength[infectee]<-infectious.period.length()
+        status.matrix$Recovery[infectee]<-current.time+status.matrix$IPLength[infectee]
+        if (runif(1)<rho){ #if symptomatic
+          status.matrix$severity[infectee]<-1
+           status.matrix$TimeSymptomOnset[infectee]<-current.time+incubation.period(status.matrix$IPLength[infectee])
+          if (runif(1)<testing.prob){
+            if (runif(1)<test.sens){
+               status.matrix$TimePosTest[infectee]<-status.matrix$TimeSymptomOnset[infectee]+test.delay
+            }
+          }
+          time.events<-rbind(time.events,c(current.time,1.1,infectee))
+        }else{
+          status.matrix$severity[infectee]<-2
+          transmission.parameters$q[infectee]<-transmission.parameters$q[infectee]*alpha.as #A single q parameter for everyone
+          time.events<-rbind(time.events,c(current.time,1.2,infectee))
+        }
+        infectives[infectee]<-1
+        contact.time$pr.ctc[infectee]<-rexp(1,transmission.parameters$contact_rate[infectee])+current.time      # I generate the next interarrival time for individual i
+      }
+    }
+    
+    
+    if (next.evts=="TestPositive"){
+      current.time<-events$TestPositive
+      testpositive.individuals<-which(testpositive.day==current.time)
+      status.matrix$TimePosTest[testpositive.individuals]<-Inf
+      for (k in testpositive.individuals){
+        transmission.parameters$contact_rate[k]<-transmission.parameters$contact_rate[k]*contact.reduction
+      }
+    }
+    
+    
+    if (next.evts=="Recovery"){
+      current.time<-events$Recovery
+      temp.recovered<-which(recovery.vector==events$Recovery)
+      for (recovered in temp.recovered){
+        status.matrix$infected[recovered]<--1
+        recovery.vector[recovered]<-Inf
+        status.matrix$Recovery[recovered]<-Inf
+        infectives[recovered]<-0
+        time.events<-rbind(time.events,c(current.time,-1,recovered))
+      }
+    }
+  }
+  
+  #When also the other pathogen is present.
+  time.events<-time.events[-1,]
+  timev.name<-c("time","event","who")
+  dimnames(time.events)<-list(NULL,timev.name)
+  
+
+  # Compute Epidemic Meausure
+  Rt<-NULL
+  first.cases<-which(status.matrix$time.of.infection==0)
+  temp.sec.cases<-NULL
+  for (j in first.cases){
+    ifelse(length(which(status.matrix$infector==j)>0),temp.sec.cases<-c(temp.sec.cases,length(which(status.matrix$infector==j))),temp.sec.cases<-c(temp.sec.cases,0))
+  }
+  Rt<-mean(temp.sec.cases)
+  
+  NewCases<-nSeeds
+  Prevalence<-nSeeds
+  last.day<-ceiling(max(time.events[,1]))
+  
+  for (i in 1:last.day){
+    temp.time<-setdiff(which(time.events[,1]>(i-1)),which(time.events[,1]>i))
+    if (length(temp.time)>0){
+      new.cases.ev<-c(which(time.events[temp.time,2]==1.1),which(time.events[temp.time,2]==1.2))
+      if (length(new.cases.ev)>0){
+        NewCases<-c(NewCases,(length(new.cases.ev)))
+        newly.infected.id<-time.events[temp.time[new.cases.ev],3]
+        temp.sec.cases<-NULL
+        for (k in newly.infected.id) {
+          ifelse(length(which(status.matrix$infector ==k)>0),temp.sec.cases<-c(temp.sec.cases,length(which(status.matrix$infector==k))),temp.sec.cases<-c(temp.sec.cases,0))
+        }
+        Rt<-c(Rt,mean(temp.sec.cases))
+      }else{
+        NewCases<-c(NewCases,0)
+        Rt<-c(Rt,NA)
+      }
+      ctime<-which(time.events[,1]<i)
+      Prevalence<-c(Prevalence, (length(which(time.events[ctime,2]==1.1))+length(which(time.events[ctime,2]==1.2))-length(which(time.events[ctime,2]==-1))))
+    }else{
+      ctime<-which(time.events[,1]<i)
+      Prevalence<-c(Prevalence, (length(which(time.events[ctime,2]==1.1))+length(which(time.events[ctime,2]==1.2))-length(which(time.events[ctime,2]==-1))))
+      NewCases<-c(NewCases,0)
+      Rt<-c(Rt,NA)
+    }
+  }
+  
+  epi.evo<-data.frame("Days"=0:last.day, "Incidence"=NewCases,"Prevalence"=Prevalence,"Rt"=Rt)
+  FinalSize<-data.frame("FinalSize"=length(which(status.matrix$infected==-1)))
+  PeakIncidence<-data.frame("PeakIncidence"=max(epi.evo$Incidence),"TimePeakIncidence"=which(epi.evo$Incidence==max(epi.evo$Incidence))[1])
+  PeakPrevalence<-data.frame("PeakPrevalence"=max(epi.evo$Prevalence),"TimePeakPrevalence"=which(epi.evo$Prevalence==max(epi.evo$Prevalence))[1])
+  
+  return(list(time.events=time.events, status.matrix=status.matrix,epi.evo=epi.evo, FinalSize=FinalSize, PeakIncidence=PeakIncidence, PeakPrevalence=PeakPrevalence))
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+

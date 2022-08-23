9b7f67a40a59881ceec9491b7b06caf486f5269e
AndreaTorneri <andrea.torneri@gmail.com>
2022-08-23 09:14:22 +0200

    "Changing the computation of the recovery event"

--- scrLeo.R
+++ scrLeo.R
@@ -140,12 +140,12 @@ sim.ekp<-function(n,prop.immune, rho,q, alpha.as,testing.prob,test.sens,test.del
     
     next.contact<-contact.time$pr.ctc #overall contact times    
     recovery.vector<-status.matrix$Recovery
+    events$Recovery<-min(recovery.vector)
     testpositive.day<-status.matrix$TimePosTest
     #Identify the next event (the one that is realized first in time)
     ifelse(length(which(is.na(next.contact)==FALSE))>0,events$NextCtc<-min(next.contact, na.rm = T),events$NextCtc<-Inf) # among all the proposed social contact between households we select the minimum
     ifelse(length(which(!is.infinite(testpositive.day)))>0,events$TestPositive<-min(testpositive.day),events$TestPositive<-Inf ) # among all the test positive day we select the minimum
-    ifelse(length(which(is.na(recovery.vector)==FALSE))>0,events$Recovery<-min(recovery.vector, na.rm = T),events$Recovery<-Inf) # among all the proposed social contact between households we select the minimum
-
+    
     next.evts<-colnames(events)[which(min(events)==events)] # if two at the same time we pick one random
     if (length(next.evts)>1){
       next.evts<-sample(colnames(events)[which(min(events)==events)],1)
